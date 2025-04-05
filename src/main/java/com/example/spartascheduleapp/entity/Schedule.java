320daa1fcb82715a8ca5cb7aeac934c2ab35992e
jinaLee11 <justonesip82@gmail.com>
2025-04-06 01:54:33 +0900

    연관관계 설정

--- src/main/java/com/example/spartascheduleapp/entity/Schedule.java
+++ src/main/java/com/example/spartascheduleapp/entity/Schedule.java
@@ -21,12 +21,17 @@ public class Schedule extends BaseEntity { //extends BaseEntity 해줘야 작성
 
     private String contents;
 
-    // JPA는 기본 생성자 필요하대용
-    // 왜 ?? JPA는 기본 생성자가 없으면 객체를 만들 수 없대 !
-    // findAll() 나 findById() 같은 메서드를 호출하면 DB에서 데이터를 꺼내와서 엔티티 객체로 바꿔줘야 하는데
-    // 그 때 내부적으로 기본 생성자를 거쳐서 동작한대 => 그러니까 꼭 필요, 없으면 에러남
+    // 연관관계 설정
+    @ManyToOne // 단방향 (schedule N : User 1)
+    @JoinColumn(name = "user_id") // user의 id를 FK로 해서 컬럼 두개 쪼인해주겠다!
+    private User user;
 
     public Schedule() {
+        /** JPA는 기본 생성자 필요하대용
+         왜 ?? JPA는 기본 생성자가 없으면 객체를 만들 수 없다 !
+         findAll() 나 findById() 같은 메서드를 호출하면 DB에서 데이터를 꺼내와서 엔티티 객체로 바꿔줘야 하는데
+         그 때 내부적으로 기본 생성자를 거쳐서 동작한대요 => 그러니까 꼭 필요, 없으면 에러남
+         */
     }
 
     public Schedule(String writer, String title, String contents) {
